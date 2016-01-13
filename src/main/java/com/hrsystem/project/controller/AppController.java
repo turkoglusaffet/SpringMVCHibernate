af17b4ae85cee3430db9a3db54a8c4c2918f1b42
turkoglusaffet <turkoglusaffet@gmail.com>
2016-01-13 20:59:23 +0200

    Update AppController.java

--- src/main/java/com/hrsystem/project/controller/AppController.java
+++ src/main/java/com/hrsystem/project/controller/AppController.java
@@ -37,9 +37,6 @@ public class AppController {
 		return "allemployees";
 	}
 
-	/*
-	 * This method will provide the medium to add a new employee.
-	 */
 	@RequestMapping(value = { "/new" }, method = RequestMethod.GET)
 	public String newEmployee(ModelMap model) {
 		Employee employee = new Employee();
@@ -48,10 +45,6 @@ public class AppController {
 		return "registration";
 	}
 
-	/*
-	 * This method will be called on form submission, handling POST request for
-	 * saving employee in database. It also validates the user input
-	 */
 	@RequestMapping(value = { "/new" }, method = RequestMethod.POST)
 	public String saveEmployee(@Valid Employee employee, BindingResult result,
 			ModelMap model) {
@@ -60,14 +53,7 @@ public class AppController {
 			return "registration";
 		}
 
-		/*
-		 * Preferred way to achieve uniqueness of field [ssn] should be implementing custom @Unique annotation 
-		 * and applying it on field [ssn] of Model class [Employee].
-		 * 
-		 * Below mentioned peace of code [if block] is to demonstrate that you can fill custom errors outside the validation
-		 * framework as well while still using internationalized messages.
-		 * 
-		 */
+	
 		if(!service.isEmployeeSsnUnique(employee.getId(), employee.getSsn())){
 			FieldError ssnError =new FieldError("employee","ssn",messageSource.getMessage("non.unique.ssn", new String[]{employee.getSsn()}, Locale.getDefault()));
 		    result.addError(ssnError);
@@ -81,9 +67,7 @@ public class AppController {
 	}
 
 
-	/*
-	 * This method will provide the medium to update an existing employee.
-	 */
+
 	@RequestMapping(value = { "/edit-{ssn}-employee" }, method = RequestMethod.GET)
 	public String editEmployee(@PathVariable String ssn, ModelMap model) {
 		Employee employee = service.findEmployeeBySsn(ssn);
@@ -92,10 +76,7 @@ public class AppController {
 		return "registration";
 	}
 	
-	/*
-	 * This method will be called on form submission, handling POST request for
-	 * updating employee in database. It also validates the user input
-	 */
+
 	@RequestMapping(value = { "/edit-{ssn}-employee" }, method = RequestMethod.POST)
 	public String updateEmployee(@Valid Employee employee, BindingResult result,
 			ModelMap model, @PathVariable String ssn) {
@@ -117,9 +98,7 @@ public class AppController {
 	}
 
 	
-	/*
-	 * This method will delete an employee by it's SSN value.
-	 */
+
 	@RequestMapping(value = { "/delete-{ssn}-employee" }, method = RequestMethod.GET)
 	public String deleteEmployee(@PathVariable String ssn) {
 		service.deleteEmployeeBySsn(ssn);
